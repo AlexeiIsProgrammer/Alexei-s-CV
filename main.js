bcb3d75a9ee1b8cc7653f7e9007f64faae6d92e0
Alexei <avenge.now.r@mail.ru>
2022-08-29 17:46:09 +0300

    add: added some animations on scrolling (part 1)

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const scrollItems = document.querySelectorAll('._anim-items');
+
+	const scrollAnimation = () => {
+		let windowCenter = (window.innerHeight) + window.scrollY;
+		scrollItems.forEach(el => {
+			let scrollOffset = el.offsetTop + (el.offsetHeight / 4);
+			if (windowCenter >= scrollOffset) {
+				el.classList.add('active-anime');
+			} else {
+				el.classList.remove('active-anime');
+			}
+		});
+	};
+    setTimeout(() => {
+        scrollAnimation();    
+    }, 300);
+	
+	window.addEventListener('scroll', () => {
+		scrollAnimation();
+	});
+});
+
+// const el = document.querySelectorAll(".tools");
+
 // setInterval(() => {
-//     console.log(screen.width)
-//     console.log(screen.height)
-// }, 1000) 
+//     el.forEach(el => {
+//         console.log(el.offsetTop);
+//     });
+// }, 1000);
