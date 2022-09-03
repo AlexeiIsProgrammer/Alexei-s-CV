eedf81ef338c3c610d23f2b3eccbc1183965e8b4
Alexei <avenge.now.r@mail.ru>
2022-09-03 20:43:55 +0300

    fix: changes typing text animation, set dependence to some figures, full white background for <768px devices

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
+let isEndGTyping = true;
+let dataText = "I believe that I'll be able to make great things in the nearest future!";
+
 document.addEventListener('DOMContentLoaded', () => {
 	const scrollItems = document.querySelectorAll('._anim-items');
 
 	const scrollAnimation = () => {
-		let windowCenter = (window.innerHeight ) + window.scrollY;
+		let windowCenter = (window.innerHeight) + window.scrollY;
 		scrollItems.forEach(el => {
 			let scrollOffset = el.offsetTop + (el.offsetHeight / 2);
 			if (windowCenter >= scrollOffset) {
 				el.classList.add('active-anime');
+				if(el.classList.contains('satodsan-uvemopag') && isEndGTyping) {
+					isEndGTyping = false;
+					StartTextAnimation(0);
+				}
 			} else {
 				el.classList.remove('active-anime');
+
+				if(el.classList.contains('satodsan-uvemopag')){
+					isEndGTyping = true;
+					StartTextAnimation(dataText.length + 1);
+				}
 			}
 
-			el.addEventListener('mouseover', function () {
-				this.classList.remove('active-anime');
-				setTimeout(()=>{
-					this.classList.add('active-anime');
-				}, 1000);
-			})
+			
 		});
 	};
     setTimeout(() => {
@@ -27,3 +34,31 @@ document.addEventListener('DOMContentLoaded', () => {
 		scrollAnimation();
 	});
 });
+
+
+
+function typeWriter(text, i, fnCallback) {
+
+	if (i < text.length) {
+
+	document.querySelector(".satodsan-uvemopag").innerHTML = text.substring(0, i+1);
+
+	setTimeout(function() {
+		typeWriter(text, i + 1, fnCallback)
+	}, 100);
+	}
+
+	else if (typeof fnCallback == 'function') {
+	setTimeout(fnCallback, 700);
+	}
+}
+
+function StartTextAnimation(i) {
+	document.querySelector(".satodsan-uvemopag").innerHTML = '';
+	if (i < dataText.length) {
+		typeWriter(dataText, 0, function(){
+			StartTextAnimation(i + 1);
+		});
+	}
+}
+
