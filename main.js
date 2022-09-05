06d63c9405393c08d5a57925732424be4a008062
Alexei <avenge.now.r@mail.ru>
2022-09-05 13:32:08 +0300

    fix: some changes at tools border, fixed horizontal scrolling

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 let isEndGTyping = true;
 let dataText = "I believe that I'll be able to make great things in the nearest future!";
 
+document.querySelectorAll('.subtitle').forEach(el => {
+	console.log(el.clientHeight);
+});
+
 document.addEventListener('DOMContentLoaded', () => {
 	const scrollItems = document.querySelectorAll('._anim-items');
 
@@ -22,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
 				}
 			}
 
-			if(el.parentElement.classList.contains('relativeFigures')) {
+			if(el.parentElement.classList.contains('relativeFigures') || el.parentElement.classList.contains('yellowBlock')) {
 				el.addEventListener('mouseover', function () {
 					this.classList.remove('active-anime');
 					setTimeout(()=>{
