02edefa826faa79bc82dc9d2cef2b0d1f0b4eb1a
Alexei <avenge.now.r@mail.ru>
2022-09-05 23:26:31 +0300

    fix: main changes in figure scroll animation is complete

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 let isEndGTyping = true;
 let dataText = "I believe that I'll be able to make great things in the nearest future!";
 
-document.querySelectorAll('.subtitle').forEach(el => {
-	console.log(el.clientHeight);
-});
+const getRelativeElement = document.querySelector('.blueWaves2');
+const getBound = getRelativeElement.getBoundingClientRect().top;
 
 document.addEventListener('DOMContentLoaded', () => {
 	const scrollItems = document.querySelectorAll('._anim-items');
@@ -11,7 +10,17 @@ document.addEventListener('DOMContentLoaded', () => {
 	const scrollAnimation = () => {
 		let windowCenter = (window.innerHeight) + window.scrollY;
 		scrollItems.forEach(el => {
-			let scrollOffset = el.offsetTop + (el.offsetHeight / 2);
+			let scrollOffset;
+			
+			if(el.parentElement.classList.contains('relativeFigures')) {
+				//console.log('Top:' + el.getBoundingClientRect().top + '---' + 'Height:' +el.offsetHeight);
+				scrollOffset = el.getBoundingClientRect().top + (el.offsetHeight * 2); //Поиграться со значениями, основную штуку зробил
+			}
+			else {
+				scrollOffset = el.offsetTop + (el.offsetHeight / 2);
+				
+			}
+
 			if (windowCenter >= scrollOffset) {
 				el.classList.add('active-anime');
 				if(el.classList.contains('satodsan-uvemopag') && isEndGTyping) {
