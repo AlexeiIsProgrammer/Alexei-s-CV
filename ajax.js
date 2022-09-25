82da6d9226f8236d9568d4e0e28856aa9b56322d
Alexei <avenge.now.r@mail.ru>
2022-09-25 13:52:46 +0300

    fix: some mail fixes

--- ajax.js
+++ ajax.js
@@ -1,24 +1,33 @@
-$( document ).ready(function() {
-    $(".submit").click(
-		function(){
-			sendAjaxForm('form', 'phpmailer/index.php');
-			return false; 
+$(document).ready(function () {
+	$("form").submit(function () {
+		let text = document.getElementById('email').value;
+		alert(text);
+		if(text!= "") {
+			// Получение ID формы
+			var formID = $(this).attr('id');
+			// Добавление решётки к имени ID
+			var formNm = $('#' + formID);
+			$.ajax({
+				type: "POST",
+				url: 'phpmailer/index.php',
+				data: formNm.serialize(),
+				beforeSend: function () {
+					// Вывод текста в процессе отправки
+					alert("Отправка");
+				},
+				success: function (data) {
+					alert("Успешно");
+				},
+				error: function (jqXHR, text, error) {
+					alert("Ошибка отправки: "+ error);
+				}
+			});
+			return false;
 		}
-	);
+		else {
+			alert('Введите почту!');
+		}
+	
+	});
 });
- 
-function sendAjaxForm(ajax_form, url) {
-    $.ajax({
-        url:     url, //url страницы (action_ajax_form.php)
-        type:     "POST", //метод отправки
-        dataType: "html", //формат данных
-        data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
-        success: function(response) { //Данные отправлены успешно
-        	result = $.parseJSON(response);
-        	alert('Mail: ' + result.mail);
-    	},
-    	error: function(response) { // Данные не отправлены
-            //alert('Ошибка. Данные не отправлены.');
-    	}
- 	});
-}
+
